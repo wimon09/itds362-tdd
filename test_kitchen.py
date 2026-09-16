a7a9aaabe248958df62ff3de9fc236d66773453c
focusberbboo <focus200249@gmail.com>
2026-09-16 20:33:30 +0700

    A4: equality, red — add equality test

--- test_kitchen.py
+++ test_kitchen.py
@@ -12,7 +12,6 @@ Test list
 from kitchen import Quantity
 
 
-def test_multiplication_returns_a_new_quantity():
-    flour = Quantity(200)
-    assert flour.times(3).amount == 600
-    assert flour.times(2).amount == 400
+def test_equality():
+    assert Quantity(200) == Quantity(200)
+    assert Quantity(200) != Quantity(300)
