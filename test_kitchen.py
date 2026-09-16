48028d6ab5a6b4c9992e1b1152b3fcc81e141429
focusberbboo <focus200249@gmail.com>
2026-09-16 20:32:08 +0700

    A3: new quantity, red — require times to return a new Quantity

--- test_kitchen.py
+++ test_kitchen.py
@@ -12,13 +12,7 @@ Test list
 from kitchen import Quantity
 
 
-def test_multiplication():
+def test_multiplication_returns_a_new_quantity():
     flour = Quantity(200)
-    flour.times(3)
-    assert flour.amount == 600
-
-
-def test_multiplication_by_two():
-    flour = Quantity(200)
-    flour.times(2)
-    assert flour.amount == 400
+    assert flour.times(3).amount == 600
+    assert flour.times(2).amount == 400
